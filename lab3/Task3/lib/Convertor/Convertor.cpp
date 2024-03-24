bc777023caa447728ff7d282a63b90a8dd05f429
muffindud <ccatlabuga2002@gmail.com>
2024-03-24 16:44:39 +0200

    add salt'n'peper filter formula

--- lab3/Task3/lib/Convertor/Convertor.cpp
+++ lab3/Task3/lib/Convertor/Convertor.cpp
@@ -7,3 +7,18 @@ float toVoltage(int reading){
 float toResistance(float voltage){
     return 10000 * voltage / (5 - voltage);
 }
+
+int saltPeperFilter(int data[SALT_PEPER_SIZE]){
+    for(int i = 0; i < SALT_PEPER_SIZE; i++){
+        for(int j = i + 1; j < SALT_PEPER_SIZE; j++){
+            if(data[i] > data[j]){
+                int temp = data[i];
+
+                data[i] = data[j];
+                data[j] = temp;
+            }
+        }
+    }
+
+    return data[(int)(SALT_PEPER_SIZE / 2)];
+}
--- lab3/Task3/lib/Convertor/Convertor.h
+++ lab3/Task3/lib/Convertor/Convertor.h
@@ -1,7 +1,10 @@
 #ifndef CONVERTOR_H
 #define CONVERTOR_H
 
+#define SALT_PEPER_SIZE 5
+
 float toVoltage(int reading);
 float toResistance(float voltage);
+int saltPeperFilter(int data[SALT_PEPER_SIZE]);
 
 #endif
