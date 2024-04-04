830f2be2a053f25e3fb97974a802700bb29b167a
muffindud <ccatlabuga2002@gmail.com>
2024-04-04 13:36:13 +0300

    implement config.h

---
+++ lab4/Task4/src/config.h
@@ -0,0 +1,15 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+    #define LCD_ADDR 0x27
+    #define LCD_COLS 16
+    #define LCD_ROWS 2
+
+    #define SERIAL_BAUD 9600
+
+    #define RELAY_IN 8
+
+    #define MOTOR_IN1 2
+    #define MOTOR_IN2 3
+    #define MOTOR_EN 9
+    #define SPEED_STEP 1
+#endif
--- lab4/Task4/src/main.cpp
+++ lab4/Task4/src/main.cpp
@@ -1,11 +1,15 @@
-#define RELAY_IN 8
-#define LCD_ADDR 0x27
-
 #include <Arduino.h>
+
 #include "Relay.h"
+#include "L298N.h"
 #include "SerialMap.h"
 
+#include "config.h"
+
 Relay relay(RELAY_IN);
+L298N motor(MOTOR_IN1, MOTOR_IN2, MOTOR_EN);
+
+String inputBuffer;
 
 void setup(){
     stdinToSerial();
