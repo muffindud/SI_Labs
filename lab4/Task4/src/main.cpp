ff4eaaec01aa934427a287af7a2912001d747c71
muffindud <ccatlabuga2002@gmail.com>
2024-04-05 09:53:09 +0300

    fix char validation

--- lab4/Task4/src/main.cpp
+++ lab4/Task4/src/main.cpp
@@ -23,6 +23,12 @@ int getPercentage(String input){
         input.remove(0, 1);
     }
 
+    for(int i = 0; i < input.length() - 1; i++){
+        if(input[i] < '0' || input[i] > '9'){
+            return -1;
+        }
+    }
+
     int percentage = input.substring(0, input.length() - 1).toInt();
 
     return negative ? -percentage : percentage;
@@ -67,6 +73,10 @@ void loop(){
         }else if(c == 127){
             if(inputBuffer.length() > 0)
                 inputBuffer.remove(inputBuffer.length() - 1);
+        }else if(c == 8){
+            if(inputBuffer.length() > 0)
+                inputBuffer.remove(inputBuffer.length() - 1);
+            c = 127;
         }else if(c != 127 && c != 13 && c != 10 && c != 8){
             inputBuffer += c;
         }
