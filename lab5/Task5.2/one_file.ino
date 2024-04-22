15ffcf50c9c01a470058a80411c2ef1225c43c00
muffindud <ccatlabuga2002@gmail.com>
2024-04-22 14:36:02 +0300

    determine kp

--- lab5/Task5.2/one_file.ino
+++ lab5/Task5.2/one_file.ino
@@ -5,15 +5,15 @@
 #define MOTOR_IN1_PIN 5
 #define MOTOR_IN2_PIN 6
 
-#define TIME_UNIT_MS 1000
 #define PWM_MIN 1
 #define PWM_MAX 255
+
 #define MAX_TPS 5400
-#define MAX_TP_TIME_UNIT (MAX_TPS * TIME_UNIT_MS / 1000)
+#define TIME_UNIT_MS 1000
 
 #define SERIAL_BAUD 9600
 
-#define KP 0.05
+#define KP 0.01
 #define KI 0.0
 #define KD 0.0
 
@@ -24,7 +24,7 @@ Encoder encoder(ENC_A_PIN, ENC_B_PIN);
 int motorPWM = 0;
 
 void setPWM(int pwm){
-    pwm = constrain(pwm, -PWM_MAX, PWM_MAX);
+    pwm = constrain(pwm, PWM_MIN, PWM_MAX);
 
     while(motorPWM != pwm){
         if(motorPWM < pwm){
@@ -32,18 +32,10 @@ void setPWM(int pwm){
         }else{
             motorPWM--;
         }
-        analogWrite(MOTOR_ENA_PIN, constrain(abs(motorPWM), PWM_MIN, PWM_MAX));
+        analogWrite(MOTOR_ENA_PIN, constrain(motorPWM, PWM_MIN, PWM_MAX));
 
-        if(motorPWM > 0){
-            digitalWrite(MOTOR_IN1_PIN, HIGH);
-            digitalWrite(MOTOR_IN2_PIN, LOW);
-        }else if(motorPWM < 0){
-            digitalWrite(MOTOR_IN1_PIN, LOW);
-            digitalWrite(MOTOR_IN2_PIN, HIGH);
-        }else if(motorPWM == 0){
-            digitalWrite(MOTOR_IN1_PIN, LOW);
-            digitalWrite(MOTOR_IN2_PIN, LOW);
-        }
+        digitalWrite(MOTOR_IN1_PIN, HIGH);
+        digitalWrite(MOTOR_IN2_PIN, LOW);
 
         // delay(10);
     }
@@ -52,12 +44,8 @@ void setPWM(int pwm){
 void applyPID(int targetTPS){
     static int lastError = 0;
     static int integral = 0;
-    targetTPS = targetTPS * TIME_UNIT_MS / 1000;
 
-    encoder.write(0);
-    delay(TIME_UNIT_MS);
     int ticks = encoder.read();
-
     int error = targetTPS - ticks;
 
     integral += error;
@@ -67,6 +55,9 @@ void applyPID(int targetTPS){
     setPWM(pwm);
 
     lastError = error;
+
+    encoder.write(0);
+    delay(TIME_UNIT_MS);
 }
 
 void setup(){
@@ -74,6 +65,7 @@ void setup(){
     pinMode(MOTOR_ENA_PIN, OUTPUT);
     pinMode(MOTOR_IN1_PIN, OUTPUT);
     pinMode(MOTOR_IN2_PIN, OUTPUT);
+    encoder.write(0);
 }
 
 void loop(){
