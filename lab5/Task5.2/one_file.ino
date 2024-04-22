3ed47fd1d503deec25af76f56479de08d55a2bb7
muffindud <ccatlabuga2002@gmail.com>
2024-04-22 13:35:49 +0300

    implement applyPID fun

--- lab5/Task5.2/one_file.ino
+++ lab5/Task5.2/one_file.ino
@@ -5,10 +5,17 @@
 #define MOTOR_IN1_PIN 5
 #define MOTOR_IN2_PIN 6
 
+#define TIME_UNIT_MS 1000
+#define PWM_MIN 1
 #define PWM_MAX 255
-#define MAX_TICKS_PER_SECOND 5400
+#define MAX_TPS 5400
+#define MAX_TP_TIME_UNIT (MAX_TPS * TIME_UNIT_MS / 1000)
 
 #define SERIAL_BAUD 9600
+
+#define KP 0.05
+#define KI 0.0
+#define KD 0.0
 
 #include <Encoder.h>
 
@@ -25,7 +32,7 @@ void setPWM(int pwm){
         }else{
             motorPWM--;
         }
-        analogWrite(MOTOR_ENA_PIN, motorPWM);
+        analogWrite(MOTOR_ENA_PIN, constrain(abs(motorPWM), PWM_MIN, PWM_MAX));
 
         if(motorPWM > 0){
             digitalWrite(MOTOR_IN1_PIN, HIGH);
@@ -38,8 +45,28 @@ void setPWM(int pwm){
             digitalWrite(MOTOR_IN2_PIN, LOW);
         }
 
-        delay(10);
+        // delay(10);
     }
+}
+
+void applyPID(int targetTPS){
+    static int lastError = 0;
+    static int integral = 0;
+    targetTPS = targetTPS * TIME_UNIT_MS / 1000;
+
+    encoder.write(0);
+    delay(TIME_UNIT_MS);
+    int ticks = encoder.read();
+
+    int error = targetTPS - ticks;
+
+    integral += error;
+    int derivative = error - lastError;
+
+    int pwm = KP * error + KI * integral + KD * derivative;
+    setPWM(pwm);
+
+    lastError = error;
 }
 
 void setup(){
@@ -50,5 +77,6 @@ void setup(){
 }
 
 void loop(){
-
+    applyPID(2700);
+    Serial.println(encoder.read());
 }
