0e57f71bc2b6466dcde03c880f862c573a9dc8ef
muffindud <ccatlabuga2002@gmail.com>
2024-05-12 14:32:35 +0300

    implement frame encode and decode

--- lab7/Task7.2.2/lib/USART_Handler/USART_Handler.cpp
+++ lab7/Task7.2.2/lib/USART_Handler/USART_Handler.cpp
@@ -1,3 +1,42 @@
 #include "USART_Handler.h"
 
+void encodeFrame(ProtocolFrame frame, byte* buffer){
+    buffer[0] = frame.STX;
+    buffer[1] = (frame.P_NR >> 8) & 0xFF;
+    buffer[2] = frame.P_NR & 0xFF;
+    buffer[3] = (frame.SRC >> 8) & 0xFF;
+    buffer[4] = frame.SRC & 0xFF;
+    buffer[5] = (frame.DST >> 8) & 0xFF;
+    buffer[6] = frame.DST & 0xFF;
+    for(int j = 0; j < sizeof(PacketType_T); j++){
+        buffer[7 + j] = ((byte*)&frame.P_ID)[j];
+    }
+    for(int j = 0; j < sizeof(Command_T); j++){
+        buffer[7 + sizeof(PacketType_T) + j] = ((byte*)&frame.CMD)[j];
+    }
+    buffer[8 + sizeof(PacketType_T) + sizeof(Command_T)] = frame.CMD;
+    buffer[9 + sizeof(PacketType_T) + sizeof(Command_T)] = (frame.DATA >> 24) & 0xFF;
+    buffer[10 + sizeof(PacketType_T) + sizeof(Command_T)] = (frame.DATA >> 16) & 0xFF;
+    buffer[11 + sizeof(PacketType_T) + sizeof(Command_T)] = (frame.DATA >> 8) & 0xFF;
+    buffer[12 + sizeof(PacketType_T) + sizeof(Command_T)] = frame.DATA & 0xFF;
+    buffer[13 + sizeof(PacketType_T) + sizeof(Command_T)] = frame.ETX;
+}
 
+void decodeFrame(byte* buffer, ProtocolFrame* frame){
+    frame->STX = buffer[0];
+    frame->P_NR = (buffer[1] << 8) | buffer[2];
+    frame->SRC = (buffer[3] << 8) | buffer[4];
+    frame->DST = (buffer[5] << 8) | buffer[6];
+    for(int j = 0; j < sizeof(PacketType_T); j++){
+        ((byte*)&frame->P_ID)[j] = buffer[7 + j];
+    }
+    for(int j = 0; j < sizeof(Command_T); j++){
+        ((byte*)&frame->CMD)[j] = buffer[7 + sizeof(PacketType_T) + j];
+    }
+    frame->DATA = 
+        buffer[9 + sizeof(PacketType_T) + sizeof(Command_T)] << 24 | 
+        buffer[10 + sizeof(PacketType_T) + sizeof(Command_T)] << 16 | 
+        buffer[11 + sizeof(PacketType_T) + sizeof(Command_T)] << 8 | 
+        buffer[12 + sizeof(PacketType_T) + sizeof(Command_T)];
+    frame->ETX = buffer[13 + sizeof(PacketType_T) + sizeof(Command_T)];
+}
--- lab7/Task7.2.2/lib/USART_Handler/USART_Handler.h
+++ lab7/Task7.2.2/lib/USART_Handler/USART_Handler.h
@@ -1,6 +1,9 @@
 #ifndef USART_HANDLER_H
 #define USART_HANDLER_H
 
+#define STX_MARK 0x02
+#define ETX_MARK 0x03
+
 #include <Arduino.h>
 
 enum PacketType_T{
@@ -18,16 +21,30 @@ enum Command_T{
 };
 
 struct ProtocolFrame{
-    unsigned short int STX = 0x02;
+    unsigned short int STX = STX_MARK;
     unsigned int P_NR;
     unsigned int SRC;
     unsigned int DST;
     PacketType_T P_ID;
     Command_T CMD;
     unsigned long int DATA;
-    unsigned short int ETX = 0x03;
+    unsigned short int ETX = ETX_MARK;
 };
 
 const int packetSize = sizeof(ProtocolFrame);
+
+/*
+    * @brief: Encode a ProtocolFrame struct into a byte array
+    * @param: ProtocolFrame frame - the frame to be encoded
+    * @param: byte* buffer - the buffer to store the encoded frame
+*/
+void encodeFrame(ProtocolFrame frame, byte* buffer);
+
+/*
+    * @brief: Decode a byte array into a ProtocolFrame struct
+    * @param: byte* buffer - the buffer to be decoded
+    * @param: ProtocolFrame* frame - the frame to store the decoded data
+*/
+void decodeFrame(byte* buffer, ProtocolFrame* frame);
 
 #endif
